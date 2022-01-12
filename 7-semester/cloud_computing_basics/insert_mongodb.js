3476fb0deef5ccfeffdf3d46b507b57bce4119b8
Ark4diiV <arkady.vashchuk@outlook.com>
2022-01-12 22:41:41 +0000

    Initial commit

---
+++ 7-semester/cloud_computing_basics/insert_mongodb.js
@@ -0,0 +1,26 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("studentsdb");
+  var myobj = [
+    { fullName: 'Иван Иванов', marks: {
+                                       'Математика': [5,4,5,5],
+	                                'Физика': [3, 4]
+	                              },
+  
+    },
+    {fullName: 'Петр Петров', marks: {
+			               'Русский язык': [2,2,3,3],
+			               'Физика': [5,4,5],
+			               'Биология':[3]
+		                      },
+    }
+  ];
+  dbo.collection("customers").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});
